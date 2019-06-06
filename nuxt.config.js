e7faf6f823c584643d0fbea19b2faf66463ea59e
Alex White <devalexwhite@gmail.com>
2019-06-06 19:35:43 -0400

    update hash

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
       {
         src: 'https://pro.fontawesome.com/releases/v5.8.2/js/all.js',
         integrity:
-          'sha384-RLPiEwcAdrH2NjFcwJipJtlFoIN1xvqPYeeDX5yYtSNu+HTIkQCDvPQ9thsUnPUS',
+          'sha384-vlOMx0hKjUCl4WzuhIhSNZSm2yQCaf0mOU1hEDK/iztH3gU4v5NMmJln9273A6Jz',
         crossorigin: 'anonymous'
       }
     ]
